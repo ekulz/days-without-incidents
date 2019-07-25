a5f44ad2442e514bc4bbc87dffd1f1ed1689d25f
lhickin <lukehickin@hotmail.com>
2019-07-25 21:16:57 +1200

    Added favicon, spinning logo, and body text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import partypopper from './party-popper.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={partypopper} className="App-logo" alt="logo" />
+        <p class="body-title">Number of days without an incident:</p>
+        <p class="body-days">9000!</p>
       </header>
     </div>
   );
