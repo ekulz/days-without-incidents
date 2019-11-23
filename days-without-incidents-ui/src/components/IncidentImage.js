ac64a6c90a2b58b7141f42f64b5d03a9ecd24902
ekulz <lukehickin@hotmail.com>
2019-11-23 16:03:12 +1300

    Add 'This Is Fine' image when an incident is running

--- days-without-incidents-ui/src/components/IncidentImage.js
+++ days-without-incidents-ui/src/components/IncidentImage.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import partypopper from '../images/party-popper.svg';
-import upsidedownface from '../images/upside-down-face.svg';
+import partyPopper from '../images/party-popper.svg';
+import upsideDownFace from '../images/upside-down-face.svg';
+import thisIsFine from '../images/this-is-fine.png';
 
 const IncidentImage = React.memo(({ apiDataJson }) => {
-    const image = apiDataJson.daysAgo === 0 ? upsidedownface : partypopper;
+    const incidentIsResolved = apiDataJson.status === "resolved"
+    const imageClassName = incidentIsResolved ? "App-logo-spin" : "App-logo-spin-circular";
+    let image = thisIsFine;
+
+    if (incidentIsResolved) {
+        image = apiDataJson.daysAgo > 0 
+            ? partyPopper
+            : upsideDownFace;
+    }
+
     return (
         <div className="incident-image">
-            <img src={image} className="App-logo-spin" alt="logo" />
+            <img src={image} className={imageClassName} alt="logo" />
         </div>
     )
 });
